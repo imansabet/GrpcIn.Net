7ddd93fe1771bb34ec75fa4551a710798ac49528
iman <dev.imansabet@gmail.com>
2024-09-07 22:24:44 +0330

    Creating a channel

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
 Console.WriteLine("Hello, World!");
+
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7135");
