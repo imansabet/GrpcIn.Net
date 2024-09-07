1e5fba313115cb4021eeaf99a1b33db6eed47879
iman <dev.imansabet@gmail.com>
2024-09-07 21:57:20 +0330

    Unary Type

--- GrpcIn.Net/Services/FirstService.cs
+++ GrpcIn.Net/Services/FirstService.cs
@@ -1,8 +1,13 @@
 ﻿using Basics;
+using Grpc.Core;
 
 namespace GrpcIn.Net.Services;
 
 public class FirstService : FirstServiceDefinition.FirstServiceDefinitionBase
 {
-    
+    public override Task<Response> Unary(Request request, ServerCallContext context)
+    {
+        var response = new Response() { Message = request.Content + "from Server " };
+        return Task.FromResult(response);
+    }
 }
