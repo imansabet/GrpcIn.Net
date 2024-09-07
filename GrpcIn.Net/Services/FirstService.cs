ed2ba98d30cea44f3dacac9cd0a88b920b0c1b7a
iman <dev.imansabet@gmail.com>
2024-09-07 22:00:50 +0330

    Client Streaming Type

--- GrpcIn.Net/Services/FirstService.cs
+++ GrpcIn.Net/Services/FirstService.cs
@@ -10,4 +10,15 @@ public class FirstService : FirstServiceDefinition.FirstServiceDefinitionBase
         var response = new Response() { Message = request.Content + "from Server " };
         return Task.FromResult(response);
     }
+    public override async Task<Response> ClientStream(IAsyncStreamReader<Request> requestStream, ServerCallContext context)
+    {
+        var response = new Response() { Message = "I Got " };
+        while( await requestStream.MoveNext())
+        {
+            var requestPayload = requestStream.Current;
+            Console.WriteLine(requestPayload);
+            response.Message = requestPayload.ToString();
+        }
+        return response;
+    }
 }
