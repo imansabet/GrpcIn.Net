eda5da30e6d2291220660214a7f5e151121690d7
iman <dev.imansabet@gmail.com>
2024-09-09 15:02:51 +0330

    create a client - Consuming a Unary Type method

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,7 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Basics;
 using Grpc.Net.Client;
 
 Console.WriteLine("Hello, World!");
 
 
-using var channel = GrpcChannel.ForAddress("https://localhost:7135");
+var option = new GrpcChannelOptions() { };
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7135" , option);
+
+var client = new FirstServiceDefinition.FirstServiceDefinitionClient(channel);
+Unary(client);
+
+
+void Unary(FirstServiceDefinition.FirstServiceDefinitionClient client)
+{
+    var request = new Request() { Content = "Hello you !" };
+    var response = client.Unary(request);
+}
